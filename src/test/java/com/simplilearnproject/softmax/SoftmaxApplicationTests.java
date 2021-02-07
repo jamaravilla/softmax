640620e31ccc469c76a1c4dc826df9497d19415d
student <jonmaravilla@gmail.com>
2021-02-07 22:04:45 +0000

    Add project skeleton

---
+++ src/test/java/com/simplilearnproject/softmax/SoftmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearnproject.softmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

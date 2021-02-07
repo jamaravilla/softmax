879af60deb65f52aa2445b5711662c473f188948
student <jonmaravilla@gmail.com>
2021-02-07 19:16:25 +0000

    Add project skeleton

---
+++ softmax/src/main/java/com/simplilearnproject/softmax/SoftmaxApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearnproject.softmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmaxApplication.class, args);
+	}
+
+}
